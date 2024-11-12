4a98c626092764b320e2f811bc2a58ff7355cf53
Nicholas Krämer <nicokraemer@gmx.net>
2024-11-12 16:07:39 +0100

    Make todos so I know where to continue

--- example.py
+++ example.py
@@ -10,8 +10,10 @@ def main(N=10, n=5, d=2):
     Qs = jax.random.normal(key, shape=(N, n, n))
     Qs = jax.vmap(lambda v: jnp.dot(v, v.T))(Qs)
 
-    Ds = jax.random.normal(key, shape=(N, n, d))
-    Ds = jax.vmap(lambda v: jnp.linalg.qr(v)[0].T)(Ds)
+
+    D = jax.random.normal(key, shape=(d, n))
+    Ds = jnp.stack([D]*N)
+
     Rs = jnp.zeros((N, d, d))
     m0 = jax.random.normal(key, shape=(n,))
     C0 = jax.random.normal(key, shape=(n, n))
@@ -21,12 +23,19 @@ def main(N=10, n=5, d=2):
 
     terminal, intermediate = jax.lax.scan(step, xs=xs, init=init)
     print(terminal[0])
+    print(Ds[-1] @ terminal[0])
+
+    # todo: assert constraint is satisfied
+
+
     # print(intermediate[1])
 
     init = init_reduce(*init, Ds[0])
     xs, (Q_para, Q_perp) = ssm_reduce(xs)
 
     terminal, intermediate = jax.lax.scan(step, xs=xs, init=init)
+
+    # todo: assert it matches filter result from above
 
     print(Q_para.shape)
     print(Q_perp.shape)
@@ -56,18 +65,24 @@ def ssm_reduce(inputs):
     _, S = jnp.linalg.qr(D.T)
     U, _ = jnp.linalg.qr(D.T, mode="complete")
 
-    V, R = jnp.linalg.qr(jnp.linalg.cholesky(Q).T @ U)
-    ndim = R.shape[0]
-    nobs = S.shape[0]
-    R_perp = R[: (ndim - nobs), : (ndim - nobs)]
-    R_para = R[(ndim - nobs) :, (ndim - nobs) :]
-    R_star = R[: (ndim - nobs), (ndim - nobs) :]
-    K = jnp.linalg.inv(R_perp) @ R_star
-
     U_para = U[:, : len(S)]
     U_perp = U[:, len(S) :]
-    Phi_perp = U_perp.T @ U @ A @ U.T @ U_perp
-    Phi_para = U_para.T @ U @ A @ U.T @ U_perp
+
+    E_para = U_para.T @ U
+    E_perp = U_perp.T @ U 
+
+
+    V, R = jnp.linalg.qr(jnp.linalg.inv(jnp.linalg.cholesky(Q).T) @ U)
+    ndim = R.shape[0]
+    nobs = S.shape[0]
+    R_perp = E_perp @ R @ E_perp.T
+    R_para = E_para @ R @ E_para.T
+    R_star = E_perp @ R @ E_para.T
+    K = jnp.linalg.inv(R_perp) @ R_star
+
+
+    Phi_perp = E_perp @ A @E_perp.T
+    Phi_para = E_para @ A @ E_perp.T
 
     A_new = Phi_perp + K @ Phi_para
     D_new = Phi_para
