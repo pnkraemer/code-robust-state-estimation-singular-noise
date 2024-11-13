a77c87337c1a33053badd2f06a247bca05181ee5
Nicholas Krämer <nicokraemer@gmx.net>
2024-11-13 14:37:01 +0100

    Collect all transformations

--- example.py
+++ example.py
@@ -1,16 +1,8 @@
-import jax
-import jax.numpy as jnp
 import functools
 from typing import NamedTuple
 
-# todo: take the Kalman filter and move more slowly.
-# 1. Take the coordinate change induced by QR(S)
-# 2. Reparametrise the whole system
-# 3. The states according to image of S should be zero
-#    The remaining states should be independent of that?
-# 4. Can we ditch states?
-# 5. Does this match what we do in maths?
-# 6. What about time-varying systems?
+import jax
+import jax.numpy as jnp
 
 
 class Transition(NamedTuple):
@@ -18,15 +10,22 @@ class Transition(NamedTuple):
     cov: jax.Array
 
 
+class Trafo(NamedTuple):
+    full: jax.Array
+    perp: jax.Array
+    para: jax.Array
+
+
 class RV(NamedTuple):
     mean: jax.Array
     cov: jax.Array
 
 
-def main(N=4, D=3, d=2):
+def main(N=4, D=3, d=1):
     # Set up config
     jax.config.update("jax_enable_x64", True)
-    jnp.set_printoptions(1)
+    jnp.set_printoptions(3)
+
     key = jax.random.PRNGKey(1)
     key1, key2, key3 = jax.random.split(key, num=3)
 
@@ -41,37 +40,38 @@ def main(N=4, D=3, d=2):
     _, solution = jax.lax.scan(step, init=init, xs=data)
 
     # Assert constraint is satisfied
-    for s, d in zip(solution.mean, data):
-        eps = jnp.sqrt(jnp.finfo(jnp.dtype(s)).eps)
-        assert jnp.allclose(constraint.linop @ s, d, rtol=eps, atol=eps)
-
-
+    for s1, s2 in zip(solution.mean, data):
+        eps = jnp.sqrt(jnp.finfo(jnp.dtype(s1)).eps)
+        assert jnp.allclose(constraint.linop @ s1, s2, rtol=eps, atol=eps)
+    print()
+    print()
+    print("-----------------------")
+    print()
+    print()
     # Change coordinates
-    Q, R = jnp.linalg.qr(constraint.linop.T, mode="complete")
+    Q, _R = jnp.linalg.qr(constraint.linop.T, mode="complete")
+    Q = Trafo(full=Q, para=Q[:, :d], perp=Q[:, d:])
     init, prior, constraint = coordinate_change(init, prior, constraint, Q=Q)
+
     step = functools.partial(filter_step, prior, constraint)
     _, solution_new = jax.lax.scan(step, init=init, xs=data)
 
     # Assert constraint is satisfied
     for m1, m2 in zip(solution_new.mean, solution.mean):
-        eps = jnp.sqrt(jnp.finfo(jnp.dtype(s)).eps)
-        assert jnp.allclose(Q @ m1, m2, rtol=eps, atol=eps)
+        eps = jnp.sqrt(jnp.finfo(jnp.dtype(m1)).eps)
+        assert jnp.allclose(Q.full @ m1, m2, rtol=eps, atol=eps)
 
-    print(constraint)
-    print(solution_new.cov)
 
 def model_prior(key, *, D) -> Transition:
     Phi = jax.random.normal(key, shape=(D, D)) / jnp.sqrt(D)
     Sigma = Phi @ Phi.T + jnp.eye(D)
     return Transition(Phi, Sigma)
-    
 
 
 def model_constraint(key, *, D, d) -> Transition:
     A = jax.random.normal(key, shape=(d, D))
     R = jnp.zeros((d, d))
     return Transition(A, R)
-    
 
 
 def model_init(key, *, D, constraint) -> RV:
@@ -80,7 +80,7 @@ def model_init(key, *, D, constraint) -> RV:
     cov = chol @ chol.T + jnp.eye(D)
     init = RV(mean, cov)
     return filter_condition(init, constraint, data=0.0)
-    
+
 
 def filter_step(prior: Transition, constraint: Transition, rv: RV, data):
     rv = filter_predict(rv, prior)
@@ -90,7 +90,6 @@ def filter_step(prior: Transition, constraint: Transition, rv: RV, data):
 
 def filter_predict(rv: RV, prior: Transition):
     m, C = rv
-
     m = prior.linop @ m
     C = prior.linop @ C @ prior.linop.T + prior.cov
     return RV(m, C)
@@ -98,22 +97,23 @@ def filter_predict(rv: RV, prior: Transition):
 
 def filter_condition(rv, constraint, data):
     S = constraint.linop @ rv.cov @ constraint.linop.T + constraint.cov
-    K = rv.cov @ constraint.linop.T @ jnp.linalg.inv(S)
+    K = jnp.linalg.solve(S.T, constraint.linop @ rv.cov.T).T
     m = rv.mean - K @ (constraint.linop @ rv.mean - data)
     C = rv.cov - K @ S @ K.T
     return RV(m, C)
 
-def coordinate_change(init, prior, constraint, *, Q):
-    m, C = init 
-    init = RV(Q.T @ m, Q.T @ C @ Q)
+
+def coordinate_change(init, prior, constraint, *, Q: Trafo):
+    m, C = init
+    init = RV(Q.full.T @ m, Q.full.T @ C @ Q.full)
 
     linop, cov = prior
-    linop = Q.T @ linop @ Q 
-    cov = Q.T @ cov @ Q
+    linop = Q.full.T @ linop @ Q.full
+    cov = Q.full.T @ cov @ Q.full
     prior = Transition(linop, cov)
 
-    linop, cov = constraint 
-    linop = linop @ Q
+    linop, cov = constraint
+    linop = linop @ Q.full
     constraint = Transition(linop, cov)
     return init, prior, constraint
 
