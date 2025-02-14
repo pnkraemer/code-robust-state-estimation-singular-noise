671977ef70c9dca82ffec4fdf4da879c0bd22e5f
Nicholas Krämer <nicokraemer@gmx.net>
2025-02-14 16:13:27 +0100

    The Kalman filter is correct

--- src/ckf/kf.py
+++ src/ckf/kf.py
@@ -4,9 +4,38 @@ import jax.numpy as jnp
 def kalman_filter():
     def kf(data, /, *, init, latent, observe):
         n, d = data.shape
-
         ms = jnp.zeros((n, d))
         cs = jnp.zeros((n, d, d))
+
+        rv = init
+        for i, d in enumerate(data):
+            rv = predict(rv, latent)
+            rv = update(rv, d, observe)
+
+            ms = ms.at[i].set(rv[0])
+            cs = cs.at[i].set(rv[1])
         return (ms, cs)
 
     return kf
+
+
+
+def predict(rv, model):
+    A, Q = model
+    m, C = rv
+
+    m = A @ m
+    C = A @ C @ A.T + Q
+    return m, C
+
+def update(rv, data, model):
+    A, Q = model
+    m, C = rv
+
+    z = A @ m
+    S = A @ C @ A.T + Q
+    K = C @ A.T @ jnp.linalg.inv(S)
+
+    m = m - K @ (z - data)
+    C = C - K @ A @ C
+    return (m, C)
--- tests/test_ckf.py
+++ tests/test_ckf.py
@@ -17,8 +17,13 @@ def test_kf_works():
     c0 = jnp.atleast_2d(1.0)
     init = (m0, c0)
 
-    data = jnp.ones((10, 1), dtype=float)
+    value = 716151.214
+    data = value * jnp.ones((10, 1), dtype=float)
     kalman = kf.kalman_filter()
     (m_all, c_all) = kalman(data, init=init, observe=observe, latent=latent)
     assert m_all.shape == data.shape
     assert c_all.shape == (*data.shape, *m0.shape)
+
+    assert jnp.allclose(m_all, value)
+    assert jnp.allclose(c_all, 0.)
+
