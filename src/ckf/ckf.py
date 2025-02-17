112413d61281e24390dd9e54285935db65878b52
Nicholas Krämer <nicokraemer@gmx.net>
2025-02-17 10:18:15 +0100

    Sharpen the constraint-test

--- src/ckf/ckf.py
+++ src/ckf/ckf.py
@@ -2,12 +2,14 @@ import jax.numpy as jnp
 import jax
 import dataclasses
 
-from typing import  Callable
+from typing import Callable
+
 
 @dataclasses.dataclass
 class RandVar:
     mean: jax.Array
     cov: jax.Array
+
 
 @dataclasses.dataclass
 class Trafo:
@@ -16,27 +18,24 @@ class Trafo:
     cov: jax.Array
 
 
-
-def condition_one_step() -> Callable:
-    def cond(*, z: RandVar, z_to_x: Trafo, x_to_y: Trafo) -> RandVar:
-        x = predict(z, z_to_x)
-        x_given_y = update(x, x_to_y)
+def filter_one_step() -> Callable:
+    def cond(*, z: RandVar, x_given_z: Trafo, y_given_x: Trafo) -> RandVar:
+        x = predict(prior=z, trafo=x_given_z)
+        x_given_y = update(prior=x, trafo=y_given_x)
         return x_given_y
 
-    def predict(rv: RandVar, model: Trafo) -> RandVar:
-        m = model.linop @ rv.mean + model.bias
-        C = model.linop @ rv.cov @ model.linop.T + model.cov
+    def predict(*, prior: RandVar, trafo: Trafo) -> RandVar:
+        m = trafo.linop @ prior.mean + trafo.bias
+        C = trafo.linop @ prior.cov @ trafo.linop.T + trafo.cov
         return RandVar(m, C)
 
-    def update(rv: RandVar, model: Trafo) -> RandVar:
-        print(model.linop.shape, rv.mean.shape, model.bias.shape)
-        z = model.linop @ rv.mean + model.bias
-        S = model.linop @ rv.cov @ model.linop.T + model.cov
-        K = rv.cov @ model.linop.T @ jnp.linalg.inv(S)
+    def update(*, prior: RandVar, trafo: Trafo) -> RandVar:
+        z = trafo.linop @ prior.mean + trafo.bias
+        S = trafo.linop @ prior.cov @ trafo.linop.T + trafo.cov
+        K = prior.cov @ trafo.linop.T @ jnp.linalg.inv(S)
 
-        m = rv.mean - K @ z
-        C = rv.cov - K @ model.linop @ rv.cov
+        m = prior.mean - K @ z
+        C = prior.cov - K @ trafo.linop @ prior.cov
         return RandVar(m, C)
 
     return cond
-
--- tests/test_ckf.py
+++ tests/test_ckf.py
@@ -2,64 +2,88 @@
 
 import jax.numpy as jnp
 from ckf import ckf
+import jax
 
+def test_one_step_condition_works(z_dim=1, x_dim=7, y_dim=5):
+    z, x_given_z, y_given_x = _model(z_shape=(z_dim,), x_shape=(x_dim,), y_shape=(y_dim,))
 
-def test_one_step_condition_works(    value = 716151.214
-):
-    m0 = jnp.asarray([1.0])
-    c0 = jnp.asarray([[1.0]])
-    z = ckf.RandVar(m0, c0)
+    filter_ = ckf.filter_one_step()
+    x_given_y = filter_(z=z, x_given_z=x_given_z, y_given_x=y_given_x)
 
-    linop = jnp.asarray([[1.0], [1.0]])
-    bias = jnp.asarray([1.0])
-    cov = jnp.asarray([[1.0, 0.1], [0.1, 10]])
-    z_to_x = ckf.Trafo(linop, bias, cov)
+    assert x_given_y.mean.shape == (x_dim,)
+    assert x_given_y.cov.shape == (x_dim, x_dim)
 
-    linop = jnp.asarray([[1.0, 0.], [0., 1.]])
-    bias = jnp.asarray([-value, 0.])
-    cov = jnp.asarray([[0., 0.], [0., 1.]])
-    x_to_y = ckf.Trafo(linop, bias, cov)
-
-    condition = ckf.condition_one_step()
-    x_given_y = condition(z=z, z_to_x=z_to_x, x_to_y=x_to_y)
-    assert x_given_y.mean.shape == z.mean.shape
-    assert x_given_y.cov.shape == z.cov.shape
-
-    assert jnp.allclose(x_given_y.mean[0], value)
-    assert jnp.allclose(x_given_y.cov[0, :], 0., atol=1e-5)
-    assert jnp.allclose(x_given_y.cov[:, 0], 0., atol=1e-5)
+    small_value = jnp.sqrt(jnp.finfo(x_given_y.mean.dtype).eps)
+    assert jnp.allclose(x_given_y.mean[z_dim:y_dim], -y_given_x.bias[z_dim:])
+    assert jnp.allclose(x_given_y.cov[z_dim:y_dim, :], 0.0, atol=small_value)
+    assert jnp.allclose(x_given_y.cov[:, z_dim:y_dim], 0.0, atol=small_value)
 
 
 
-def test_model_reduce(    value = 716151.214
-):
-    m0 = jnp.asarray([1.0])
-    c0 = jnp.asarray([[1.0]])
-    z = ckf.RandVar(m0, c0)
+def test_model_reduce(value=716151.214):
+    z, x_given_z, y_given_x = _model(value=value)
 
-    linop = jnp.asarray([[1.0], [1.0]])
-    bias = jnp.asarray([1.0])
-    cov = jnp.asarray([[1.0, 0.1], [0.1, 10]])
-    z_to_x = ckf.Trafo(linop, bias, cov)
+    # todo: implement the recursion from the paper draft
+    # (but ignore all z1's and thus think of 'z2' as 'z')
+    out = model_reduce(z=z, x_given_z=z_to_x, y_given_x=x_to_y)
+    y2_given_z, x2_given_z, y1_given_x2, x_from_xs = out
 
-    linop = jnp.asarray([[1.0, 0.], [0., 1.]])
-    bias = jnp.asarray([-value, 0.])
-    cov = jnp.asarray([[0., 0.], [0., 1.]])
-    x_to_y = ckf.Trafo(linop, bias, cov)
+    # Assert shapes
 
-    out = model_reduce(z=z, z_to_x=z_to_x, x_to_y=x_to_y)
-    z_to_y2, z_to_x2, x2_to_y1, x1_value = out
-
-    assert z_to_y2.linop.shape == (1, 2)
+    assert z_to_y2.linop.shape == (1, 1)
     assert z_to_y2.bias.shape == (1,)
     assert z_to_y2.cov.shape == (1, 1)
 
-    assert z_to_x2.linop.shape == (2, 1)
-    assert z_to_x2.bias.shape == (2, )
-    assert z_to_x2.cov.shape == (2, 2)
+    assert z_to_x2.linop.shape == (1, 1)
+    assert z_to_x2.bias.shape == (1,)
+    assert z_to_x2.cov.shape == (1, 1)
 
-    assert x2_to_y1.linop.shape == (1, 2)
+    assert x2_to_y1.linop.shape == (1, 1)
     assert x2_to_y1.bias.shape == (1,)
     assert x2_to_y1.cov.shape == (1, 1)
 
     assert x2_value.shape == (1,)
+    assert jnp.allclose(x2_value, value)
+
+    # Assert values:
+    z_given_y2, y2 = condition(likelihood=y2_given_z, prior=z)
+
+    # From here on, we filter
+    # Predict:
+    x2 = marginal(x2_given_z, z_given_y2)
+
+    # Update
+    x2_given_y1, y1 = condition(likelihood=y_given_x2, prior=x2)
+
+    # x2 is the new z. Get new models and repeat:
+    # z = x2_given_y1
+    # x_given_z = merge(x_given_z, x_from_xs)
+    # Repeat...
+
+    # Reconstruct x:
+    x = marginal(x_from_xs, x2_given_y1)
+
+    # Compare this x to that of the traditional Kalman filter
+    assert jnp.allclose(x.mean, x_ref.mean)
+    assert jnp.allclose(x.cov, x_ref.cov)
+
+
+def _model(*, z_shape, x_shape, y_shape):
+    key = jax.random.PRNGKey(seed=2)
+
+    key, k1, k2 = jax.random.split(key, num=3)
+    m0 = jax.random.normal(k1, shape=z_shape)
+    c0 = jax.random.normal(k2, shape=(*z_shape, *z_shape))
+    z = ckf.RandVar(m0, c0 @ c0.T)
+
+    key, k1, k2, k3 = jax.random.split(key, num=4)
+    linop = jax.random.normal(k1, shape=(*x_shape, *z_shape))
+    bias = jax.random.normal(k2, shape=x_shape)
+    cov = jax.random.normal(k3, shape=(*x_shape, *x_shape))
+    x_given_z = ckf.Trafo(linop, bias, cov @ cov.T)
+
+    bias = jax.random.normal(key, shape=y_shape)
+    linop = jnp.eye(*y_shape, *x_shape)
+    cov = jnp.eye(*y_shape, *z_shape)
+    y_given_x = ckf.Trafo(linop, bias, cov @ cov.T)
+    return z, x_given_z, y_given_x
