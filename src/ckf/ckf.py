34bbde87fe79524b62b4e9ebe3c25736db6ed20d
Nicholas Krämer <nicokraemer@gmx.net>
2025-02-18 14:10:13 +0100

    Isolate covariance logic in an Impl object

--- src/ckf/ckf.py
+++ src/ckf/ckf.py
@@ -2,33 +2,95 @@ import jax.numpy as jnp
 import jax
 import dataclasses
 
-from typing import Callable
+from typing import Callable, Generic, TypeVar
+
+T = TypeVar("T")
+
+
+@dataclasses.dataclass
+class Impl:
+    rv_from_cholesky: Callable
+    trafo_factorise: Callable
+    combine: Callable
+    evaluate_conditional: Callable
+    condition: Callable
+    marginal: Callable
+
+
+def impl_cov_based():
+    @jax.tree_util.register_dataclass
+    @dataclasses.dataclass
+    class RandVar:
+        mean: jax.Array
+        cov: jax.Array
+
+        def __rmatmul__(self, other: jax.Array):
+            """Implement Array @ Trafo"""
+            mean = other @ self.mean
+            cov = other @ self.cov @ other.T
+            return RandVar(mean, cov)
+
+    def rv_from_cholesky(m, c):
+        return RandVar(m, c @ c.T)
+
+    def trafo_factorise(*, trafo: Trafo, index: int) -> tuple[Trafo, SplitTrafo]:
+        cov1, cov2 = jnp.split(trafo.noise.cov, indices_or_sections=[index], axis=0)
+        C1, C21 = jnp.split(cov1, indices_or_sections=[index], axis=1)
+        C12, C2 = jnp.split(cov2, indices_or_sections=[index], axis=1)
+
+        G = C12 @ jnp.linalg.inv(C1)
+        Z = C2 - G @ C1 @ G.T
+
+        bias1, bias2 = jnp.split(trafo.noise.mean, indices_or_sections=[index], axis=0)
+        linop1, linop2 = jnp.split(trafo.linop, indices_or_sections=[index], axis=0)
+
+        x1_mid_z = Trafo(linop1, RandVar(bias1, C1))
+        x1_mid_x2_and_z = SplitTrafo(
+            G, linop2 - G @ linop1, RandVar(bias2 - G @ bias1, Z)
+        )
+        return x1_mid_z, x1_mid_x2_and_z
+
+    def combine(*, outer: Trafo[T], inner: Trafo[T]) -> Trafo[T]:
+        linop = outer.linop @ inner.linop
+        bias = outer.linop @ inner.noise.mean + outer.noise.mean
+        cov = outer.linop @ inner.noise.cov @ outer.linop.T + outer.noise.cov
+        return Trafo(linop, RandVar(bias, cov))
+
+    def evaluate_conditional(data, *, trafo: Trafo[T]) -> T:
+        mean = trafo.linop @ data + trafo.noise.mean
+        return RandVar(mean=mean, cov=trafo.noise.cov)
+
+    def condition(*, prior: T, trafo: Trafo):
+        z = trafo.linop @ prior.mean + trafo.noise.mean
+        S = trafo.linop @ prior.cov @ trafo.linop.T + trafo.noise.cov
+        marg = RandVar(z, S)
+
+        K = prior.cov @ trafo.linop.T @ jnp.linalg.inv(S)
+        m = prior.mean - K @ z
+        C = prior.cov - K @ trafo.linop @ prior.cov
+        cond = Trafo(linop=K, noise=RandVar(mean=m, cov=C))
+        return marg, cond
+
+    def marginal(*, prior: T, trafo: Trafo) -> T:
+        m = trafo.linop @ prior.mean + trafo.noise.mean
+        C = trafo.linop @ prior.cov @ trafo.linop.T + trafo.noise.cov
+        return RandVar(m, C)
+
+    return Impl(
+        rv_from_cholesky=rv_from_cholesky,
+        trafo_factorise=trafo_factorise,
+        combine=combine,
+        evaluate_conditional=evaluate_conditional,
+        condition=condition,
+        marginal=marginal,
+    )
 
 
 @jax.tree_util.register_dataclass
 @dataclasses.dataclass
-class RandVar:
-    mean: jax.Array
-    cov: jax.Array
-
-
-@jax.tree_util.register_dataclass
-@dataclasses.dataclass
-class Trafo:
+class Trafo(Generic[T]):
     linop: jax.Array
-    noise: RandVar
-
-    def cov_to_cholesky(self):
-        eigh = jnp.linalg.eigh(self.cov)
-        cholesky = eigh.eigenvectors @ jnp.sqrt(jnp.diag(eigh.eigenvalues))
-        return jnp.linalg.qr(cholesky.T, mode="r").T
-
-    def cov_to_low_rank(self):
-        eigh = jnp.linalg.eigh(self.cov)
-        cholesky = eigh.eigenvectors @ jnp.sqrt(jnp.diag(eigh.eigenvalues))
-
-        small = jnp.finfo(cholesky.dtype).eps
-        return cholesky[:, eigh.eigenvalues > small]
+    noise: T
 
     def __matmul__(self, other: jax.Array):
         """Implement Trafo @ Array"""
@@ -36,48 +98,15 @@ class Trafo:
 
     def __rmatmul__(self, other: jax.Array):
         """Implement Array @ Trafo"""
-        mean = other @ self.noise.mean
-        cov = other @ self.noise.cov @ other.T
-        noise = RandVar(mean, cov)
-        return Trafo(linop=other @ self.linop, noise=noise)
+        return Trafo(linop=other @ self.linop, noise=other @ self.noise)
 
 
 @jax.tree_util.register_dataclass
 @dataclasses.dataclass
-class SplitTrafo:
+class SplitTrafo(Generic[T]):
     linop1: jax.Array
     linop2: jax.Array
-    noise: RandVar
-
-
-def marginal(*, prior: RandVar, trafo: Trafo) -> RandVar:
-    m = trafo.linop @ prior.mean + trafo.noise.mean
-    C = trafo.linop @ prior.cov @ trafo.linop.T + trafo.noise.cov
-    return RandVar(m, C)
-
-
-def condition(*, prior: RandVar, trafo: Trafo):
-    z = trafo.linop @ prior.mean + trafo.noise.mean
-    S = trafo.linop @ prior.cov @ trafo.linop.T + trafo.noise.cov
-    marg = RandVar(z, S)
-
-    K = prior.cov @ trafo.linop.T @ jnp.linalg.inv(S)
-    m = prior.mean - K @ z
-    C = prior.cov - K @ trafo.linop @ prior.cov
-    cond = Trafo(linop=K, noise=RandVar(mean=m, cov=C))
-    return marg, cond
-
-
-def evaluate_conditional(data, *, trafo: Trafo) -> RandVar:
-    mean = trafo.linop @ data + trafo.noise.mean
-    return RandVar(mean=mean, cov=trafo.noise.cov)
-
-
-def combine(*, outer: Trafo, inner: Trafo) -> Trafo:
-    linop = outer.linop @ inner.linop
-    bias = outer.linop @ inner.noise.mean + outer.noise.mean
-    cov = outer.linop @ inner.noise.cov @ outer.linop.T + outer.noise.cov
-    return Trafo(linop, RandVar(bias, cov))
+    noise: T
 
 
 def trafo_split(*, trafo: Trafo, index: int) -> SplitTrafo:
@@ -85,36 +114,21 @@ def trafo_split(*, trafo: Trafo, index: int) -> SplitTrafo:
     return SplitTrafo(linop1, linop2, trafo.noise)
 
 
-def trafo_factorise(*, trafo: Trafo, index: int) -> tuple[Trafo, SplitTrafo]:
-    cov1, cov2 = jnp.split(trafo.noise.cov, indices_or_sections=[index], axis=0)
-    C1, C21 = jnp.split(cov1, indices_or_sections=[index], axis=1)
-    C12, C2 = jnp.split(cov2, indices_or_sections=[index], axis=1)
-
-    G = C12 @ jnp.linalg.inv(C1)
-    Z = C2 - G @ C1 @ G.T
-
-    bias1, bias2 = jnp.split(trafo.noise.mean, indices_or_sections=[index], axis=0)
-    linop1, linop2 = jnp.split(trafo.linop, indices_or_sections=[index], axis=0)
-
-    x1_mid_z = Trafo(linop1, RandVar(bias1, C1))
-    x1_mid_x2_and_z = SplitTrafo(G, linop2 - G @ linop1, RandVar(bias2 - G @ bias1, Z))
-    return x1_mid_z, x1_mid_x2_and_z
-
-
 def invert_deterministic(y: jax.Array, /, *, deterministic_trafo) -> jax.Array:
-    return jnp.linalg.solve(
-        deterministic_trafo.linop, y - deterministic_trafo.noise.mean
-    )
+    A = deterministic_trafo.linop
+    b = y - deterministic_trafo.noise.mean
+    return jnp.linalg.solve(A, b)
 
 
-def fix_x1(x1: jax.Array, /, *, split_trafo):
+def fix_x1(x1: jax.Array, /, *, split_trafo, impl):
     linop = split_trafo.linop2
-    bias = split_trafo.linop1 @ x1 + split_trafo.noise.mean
-    cov = split_trafo.noise.cov
-    return Trafo(linop, RandVar(bias, cov))
+
+    trafo = Trafo(split_trafo.linop1, split_trafo.noise)
+    noise = impl.evaluate_conditional(x1, trafo=trafo)
+    return Trafo(linop, noise)
 
 
-def model_reduce(*, y_mid_x: Trafo, x_mid_z: Trafo, F):
+def model_reduce(*, y_mid_x: Trafo, x_mid_z: Trafo, F, impl):
     # First QR iteration
     _, ndim = F.shape
     V, R = jnp.linalg.qr(F, mode="complete")
@@ -128,7 +142,9 @@ def model_reduce(*, y_mid_x: Trafo, x_mid_z: Trafo, F):
 
     # Factorise the z-to-x conditional
     x1_and_x2_mid_z = W.T @ x_mid_z
-    x1_mid_z, x2_mid_x1_and_z = trafo_factorise(trafo=x1_and_x2_mid_z, index=len(V2.T))
+    x1_mid_z, x2_mid_x1_and_z = impl.trafo_factorise(
+        trafo=x1_and_x2_mid_z, index=len(V2.T)
+    )
 
     # y2 | x1 is deterministic (ie zero cov) and y2 is independent of x2 given x1
     y2_mid_x1 = y2_mid_x @ W1
@@ -138,10 +154,13 @@ def model_reduce(*, y_mid_x: Trafo, x_mid_z: Trafo, F):
     y1_mid_x1_and_x2 = trafo_split(trafo=(y1_mid_x @ W), index=len(V2.T))
 
     # We need to memorise how to turn x1/x2 back into x
-    x_mid_x1_and_x2 = trafo_split(trafo=Trafo(W, RandVar(0.0, 0.0)), index=len(V2.T))
+    trafo = Trafo(
+        W, impl.rv_from_cholesky(jnp.zeros((len(W),)), jnp.zeros((len(W), len(W))))
+    )
+    x_mid_x1_and_x2 = trafo_split(trafo=trafo, index=len(V2.T))
 
     # We only care about y2 | z, not about x1 | z, so we combine transformations
-    y2_mid_z = combine(outer=y2_mid_x1, inner=x1_mid_z)
+    y2_mid_z = impl.combine(outer=y2_mid_x1, inner=x1_mid_z)
 
     # Return values:
     reduced_model = (y2_mid_z, x2_mid_x1_and_z, y1_mid_x1_and_x2)
@@ -152,9 +171,8 @@ def model_reduce(*, y_mid_x: Trafo, x_mid_z: Trafo, F):
     return reduced_model, info
 
 
-def model_reduced_apply(y: jax.Array, *, z, reduced):
+def model_reduced_apply(y: jax.Array, *, z, reduced, impl):
     # todo: isolate all covariance logic:
-    #  - make trafo a linop/noise combo, and noise is a RandVar
     #  - separate covariance-affected computation from the other
     #  - make the rank of F an argument somewhere
     #  - test corner cases of fully constrained and unconstrained estimation
@@ -176,17 +194,17 @@ def model_reduced_apply(y: jax.Array, *, z, reduced):
     #  Recall that by construction of the QR decompositions,
     #  y2_mid_x1 has zero covariance.
     x1_value = invert_deterministic(y2, deterministic_trafo=y2_mid_x1)
-    x2_mid_z = fix_x1(x1_value, split_trafo=x2_mid_x1_and_z)
-    y1_mid_x2 = fix_x1(x1_value, split_trafo=y1_mid_x1_and_x2)
-    x_mid_x2 = fix_x1(x1_value, split_trafo=x_mid_x1_and_x2)
+    x2_mid_z = fix_x1(x1_value, split_trafo=x2_mid_x1_and_z, impl=impl)
+    y1_mid_x2 = fix_x1(x1_value, split_trafo=y1_mid_x1_and_x2, impl=impl)
+    x_mid_x2 = fix_x1(x1_value, split_trafo=x_mid_x1_and_x2, impl=impl)
 
     # Fix y2 in the "prior" distribution
-    _y2, z_mid_y2 = condition(prior=z, trafo=y2_mid_z)
-    z = evaluate_conditional(y2, trafo=z_mid_y2)
+    _y2, z_mid_y2 = impl.condition(prior=z, trafo=y2_mid_z)
+    z = impl.evaluate_conditional(y2, trafo=z_mid_y2)
 
     # Now we have z, x2_mid_z, and y1_mid_x2
     # which is a "complete model" and we can run the usual estimation
-    x2 = marginal(prior=z, trafo=x2_mid_z)
-    _y1, backward = condition(prior=x2, trafo=y1_mid_x2)
-    x2_mid_y1 = evaluate_conditional(y1, trafo=backward)
+    x2 = impl.marginal(prior=z, trafo=x2_mid_z)
+    _y1, backward = impl.condition(prior=x2, trafo=y1_mid_x2)
+    x2_mid_y1 = impl.evaluate_conditional(y1, trafo=backward)
     return x2_mid_y1, x_mid_x2
--- tests/test_ckf.py
+++ tests/test_ckf.py
@@ -4,11 +4,44 @@ import jax.numpy as jnp
 from ckf import ckf
 import jax
 
-jax.config.update("jax_enable_x64", True)
+
+def test_model_align_shapes(z_dim=3, x_dim=15, y_dim=(2, 7)):
+    impl = ckf.impl_cov_based()
+    y, (z, x_mid_z, y_mid_x), F = _model_random(
+        dim_z=z_dim, dim_x=x_dim, dim_y=y_dim, impl=impl
+    )
+
+    reduced = ckf.model_reduce(y_mid_x=y_mid_x, x_mid_z=x_mid_z, F=F, impl=impl)
+    x2_mid_data, x_mid_x2 = ckf.model_reduced_apply(y, z=z, reduced=reduced, impl=impl)
+
+    x2_dim = x_dim - y_dim[0]
+    assert x2_mid_data.mean.shape == (x2_dim,)
+    assert x2_mid_data.cov.shape == (x2_dim, x2_dim)
+
+
+def test_model_align_values(z_dim=1, x_dim=6, y_dim=(1, 2)):
+    impl = ckf.impl_cov_based()
+    y, (z, x_mid_z, y_mid_x), F = _model_random(
+        dim_z=z_dim, dim_x=x_dim, dim_y=y_dim, impl=impl
+    )
+
+    # Reference:
+    ref_x = impl.marginal(prior=z, trafo=x_mid_z)
+    _y, ref_backward = impl.condition(prior=ref_x, trafo=y_mid_x)
+    ref_x_mid_y = impl.evaluate_conditional(y, trafo=ref_backward)
+
+    # Reduced model:
+    reduced = ckf.model_reduce(y_mid_x=y_mid_x, x_mid_z=x_mid_z, F=F, impl=impl)
+    x2_mid_data, x_mid_x2 = ckf.model_reduced_apply(y, z=z, reduced=reduced, impl=impl)
+
+    x_mid_data = impl.marginal(prior=x2_mid_data, trafo=x_mid_x2)
+
+    assert jnp.allclose(x_mid_data.mean, ref_x_mid_y.mean)
 
 
 def test_kalman_filter():
-    (z, x_mid_z, y_mid_x), F = _model_interpolation()
+    impl = ckf.impl_cov_based()
+    (z, x_mid_z, y_mid_x), F = _model_interpolation(impl=impl)
 
     data_in = jnp.linspace(0, 1, num=20)
     data_out = data_in + 0.1 + jnp.sin(data_in**2)
@@ -17,10 +50,10 @@ def test_kalman_filter():
     x = z
 
     for d in data_out:
-        x = ckf.marginal(prior=x, trafo=x_mid_z)
-        _, x = ckf.condition(prior=x, trafo=y_mid_x)
+        x = impl.marginal(prior=x, trafo=x_mid_z)
+        _, x = impl.condition(prior=x, trafo=y_mid_x)
 
-        x = ckf.evaluate_conditional(jnp.atleast_1d(d), trafo=x)
+        x = impl.evaluate_conditional(jnp.atleast_1d(d), trafo=x)
         means.append(x.mean)
         covs.append(x.cov)
 
@@ -35,9 +68,9 @@ def test_kalman_filter():
 
     for d in data_out:
         d = jnp.atleast_1d(d)
-        reduced = ckf.model_reduce(y_mid_x=y_mid_x, x_mid_z=x_mid_z, F=F)
-        x2, x_mid_x2 = ckf.model_reduced_apply(d, z=z, reduced=reduced)
-        z = ckf.marginal(prior=x2, trafo=x_mid_x2)
+        reduced = ckf.model_reduce(y_mid_x=y_mid_x, x_mid_z=x_mid_z, F=F, impl=impl)
+        x2, x_mid_x2 = ckf.model_reduced_apply(d, z=z, reduced=reduced, impl=impl)
+        z = impl.marginal(prior=x2, trafo=x_mid_x2)
 
         means.append(z.mean)
         covs.append(z.cov)
@@ -47,81 +80,42 @@ def test_kalman_filter():
     assert jnp.allclose(jnp.stack(covs)[:, :, 0], 0.0, atol=1e-5)
 
 
-def test_filter_one_step_works(z_dim=1, x_dim=9, y_dim=(2, 5)):
-    data, (z, x_mid_z, y_mid_x), _ = _model_random(
-        dim_z=z_dim, dim_x=x_dim, dim_y=y_dim
-    )
-
-    x = ckf.marginal(prior=z, trafo=x_mid_z)
-    _y, backward = ckf.condition(prior=x, trafo=y_mid_x)
-    x_mid_y = ckf.evaluate_conditional(data, trafo=backward)
-
-    assert x_mid_y.mean.shape == (x_dim,)
-    assert x_mid_y.cov.shape == (x_dim, x_dim)
-
-    rank = x_dim - y_dim[0]  # xdim - singular dim
-    assert jnp.linalg.matrix_rank(x_mid_y.cov) == rank
-
-
-def test_model_align_shapes(z_dim=3, x_dim=15, y_dim=(2, 7)):
-    y, (z, x_mid_z, y_mid_x), F = _model_random(dim_z=z_dim, dim_x=x_dim, dim_y=y_dim)
-
-    reduced = ckf.model_reduce(y_mid_x=y_mid_x, x_mid_z=x_mid_z, F=F)
-    x2_mid_data, x_mid_x2 = ckf.model_reduced_apply(y, z=z, reduced=reduced)
-
-    x2_dim = x_dim - y_dim[0]
-    assert x2_mid_data.mean.shape == (x2_dim,)
-    assert x2_mid_data.cov.shape == (x2_dim, x2_dim)
-
-
-def test_model_align_values(z_dim=1, x_dim=6, y_dim=(1, 2)):
-    y, (z, x_mid_z, y_mid_x), F = _model_random(dim_z=z_dim, dim_x=x_dim, dim_y=y_dim)
-
-    # Reference:
-    ref_x = ckf.marginal(prior=z, trafo=x_mid_z)
-    _y, ref_backward = ckf.condition(prior=ref_x, trafo=y_mid_x)
-    ref_x_mid_y = ckf.evaluate_conditional(y, trafo=ref_backward)
-
-    # Reduced model:
-    reduced = ckf.model_reduce(y_mid_x=y_mid_x, x_mid_z=x_mid_z, F=F)
-    x2_mid_data, x_mid_x2 = ckf.model_reduced_apply(y, z=z, reduced=reduced)
-
-    x_mid_data = ckf.marginal(prior=x2_mid_data, trafo=x_mid_x2)
-
-    assert jnp.allclose(x_mid_data.mean, ref_x_mid_y.mean)
-
-
-def _model_interpolation():
+def _model_interpolation(impl):
     m0 = jnp.zeros((2,))
     c0 = jnp.eye(2)
-    z = ckf.RandVar(m0, c0)
+    z = impl.rv_from_cholesky(m0, c0)
 
     linop = jnp.eye(2)
     bias = jnp.zeros((2,))
     cov = jnp.eye(2)
-    x_mid_z = ckf.Trafo(linop, ckf.RandVar(bias, cov))
+    noise = impl.rv_from_cholesky(bias, cov)
+    x_mid_z = ckf.Trafo(linop, noise)
 
     linop = jnp.eye(1, 2)
     bias = jnp.zeros((1,))
     cov = jnp.zeros((1, 1))
-    y_mid_x = ckf.Trafo(linop, ckf.RandVar(bias, cov))
+    noise = impl.rv_from_cholesky(bias, cov)
+    y_mid_x = ckf.Trafo(linop, noise)
 
     return (z, x_mid_z, y_mid_x), cov
 
 
-def _model_random(*, dim_z, dim_x, dim_y):
+def _model_random(*, dim_z, dim_x, dim_y, impl):
     key = jax.random.PRNGKey(seed=3)
 
     key, k1, k2 = jax.random.split(key, num=3)
     m0 = jax.random.normal(k1, shape=(dim_z,))
     c0 = jax.random.normal(k2, shape=(dim_z, dim_z))
-    z = ckf.RandVar(m0, c0 @ c0.T)
+    z = impl.rv_from_cholesky(m0, c0)
+
+    # z = ckf.RandVar(m0, c0 @ c0.T)
 
     key, k1, k2, k3 = jax.random.split(key, num=4)
     linop = jax.random.normal(k1, shape=(dim_x, dim_z))
     bias = jax.random.normal(k2, shape=(dim_x,))
     cov = jax.random.normal(k3, shape=(dim_x, dim_x))
-    x_mid_z = ckf.Trafo(linop, ckf.RandVar(bias, cov @ cov.T))
+    noise = impl.rv_from_cholesky(bias, cov)
+    x_mid_z = ckf.Trafo(linop, noise)
 
     key, k1, k2, k3 = jax.random.split(key, num=4)
     dim_y_sing, dim_y_nonsing = dim_y
@@ -131,7 +125,8 @@ def _model_random(*, dim_z, dim_x, dim_y):
     linop = jax.random.normal(k1, shape=(dim_y_total, dim_x))
     bias = jax.random.normal(k2, shape=(dim_y_total,))
     F = jax.random.normal(k3, shape=(dim_y_total, dim_y_nonsing))
-    y_mid_x = ckf.Trafo(linop, ckf.RandVar(bias, F @ F.T))
+    noise = impl.rv_from_cholesky(bias, F)
+    y_mid_x = ckf.Trafo(linop, noise)
 
     data = jax.random.normal(key, shape=(dim_y_total,))
     return data, (z, x_mid_z, y_mid_x), F
